9be4a4c7e0ab78c91aef77dcbf14cf9d67c8341f
nikhil-kamboj420 <kambojnikhil44@gmail.com>
2025-04-02 23:45:22 +0530

    add contact section

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,44 +1,57 @@
+import { useState } from "react";
+import { Popup } from "../components/UI/Popup";
 export const Contact = () => {
-
-    const handleFormSubmit = (formData)=>{
-   const formInputData = Object.fromEntries(formData.entries());
-    }
+  const [formData, setFormData] = useState("");
+  const [showPopup, setShowPopup] = useState(false);
+  const sectionStyles = "section-contact";
+  const handleFormSubmit = (formData) => {
+    const formInputData = Object.fromEntries(formData.entries());
+    setFormData(formInputData);
+    setShowPopup(true);
+  };
 
   return (
-    <section className="section-contact">
-      <h2 className="container-title">Contact Us</h2>
+    <>
+      <Popup
+        formData={formData}
+        showPopup={showPopup}
+        setShowPopup={setShowPopup}
+      />
+      <section className={showPopup ? "blur" : sectionStyles}>
+        <h2 className="container-title">Contact Us</h2>
 
-      <div className="contact-wrapper container">
-      <form action={handleFormSubmit}>
-        <input
-          type="text"
-          className="form-control"
-          name="username"
-          placeholder="Enter your name"
-          required
-          autoComplete="false"
-        />
-        <input
-          type="email"
-          className="form-control"
-          name="email"
-          placeholder="Enter your email"
-          required
-          autoComplete="false"
-        />
-        <textarea
-          rows="10"
-          className="form-control"
-          name="message"
-          placeholder="Enter your message"
-          required
-          autoComplete="false"
-        ></textarea>
-        <button type="submit" className="btn btn-primary" value="send">
-          Send
-        </button>
-      </form>
-      </div>
-    </section>
+        <div className="contact-wrapper container">
+          <form action={handleFormSubmit}>
+            <input
+              type="text"
+              className="form-control"
+              name="username"
+              placeholder="Enter your name"
+              required
+              autoComplete="off"
+            />
+            <input
+              type="email"
+              className="form-control"
+              name="email"
+              placeholder="Enter your email"
+              required
+              autoComplete="off"
+            />
+            <textarea
+              rows="10"
+              className="form-control"
+              name="message"
+              placeholder="Enter your message"
+              required
+              autoComplete="off"
+            ></textarea>
+            <button type="submit" className="btn btn-primary" value="send">
+              Send
+            </button>
+          </form>
+        </div>
+      </section>
+    </>
   );
 };
